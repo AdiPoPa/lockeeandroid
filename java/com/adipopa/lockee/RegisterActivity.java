b47b5149222c447cb85dcf6508ed31772868d303
Adrian Popa <adipopa9910@gmail.com>
2016-07-17 15:57:42 +0300

    Update RegisterActivity.java

--- java/com/adipopa/lockee/RegisterActivity.java
+++ java/com/adipopa/lockee/RegisterActivity.java
@@ -1,45 +1,35 @@
 package com.adipopa.lockee;
 
-import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.TypedValue;
+import android.view.View;
 import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    public static EditText firstNameField, lastNameField, emailField, passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_screen);
 
-        // Convert DIP to pixels
+        firstNameField = (EditText)findViewById(R.id.firstNameField);
+        lastNameField = (EditText)findViewById(R.id.lastNameField);
+        emailField = (EditText)findViewById(R.id.emailField);
+        passwordField = (EditText)findViewById(R.id.passwordField);
 
-        Resources r = getResources();
-        int width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 400,
-                r.getDisplayMetrics()
-        );
 
-        int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 34,
-                r.getDisplayMetrics()
-        );
+    }
 
-        // Set every field's width by pixels
+    public void onRegister(View view){
+        String firstName = firstNameField.getText().toString();
+        String lastName = lastNameField.getText().toString();
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+        String type = "register";
 
-        EditText firstNameField = (EditText)findViewById(R.id.firstNameField);
-        firstNameField.setWidth(width);
-        firstNameField.setHeight(height);
-
-        EditText lastNameField = (EditText)findViewById(R.id.lastNameField);
-        lastNameField.setWidth(width);
-        lastNameField.setHeight(height);
-
-        EditText emailField = (EditText)findViewById(R.id.emailField);
-        emailField.setWidth(width);
-        emailField.setHeight(height);
-
-        EditText passwordField = (EditText)findViewById(R.id.passwordField);
-        passwordField.setWidth(width);
-        passwordField.setHeight(height);
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, firstName, lastName, email, password);
     }
 }
