565dffa9dc0b11af70f0a1b66f467991e3ea4ea9
Adrian Popa <adipopa9910@gmail.com>
2016-08-09 00:15:27 +0300

    Added request and display of the user's locks list
    Also an additional button for switching between states

--- app/src/main/java/com/adipopa/lockee/MainActivity.java
+++ app/src/main/java/com/adipopa/lockee/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -18,16 +19,24 @@ import android.widget.Toast;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static String registerNotification = "hide";
     TextView registerNotif;
+    Button switchButton;
+    ListView mainList;
+    TextView noLocks;
+    List<Data> dataList;
+    Data[] simpleArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         if(LoginActivity.active) {
             LoginActivity.login.finish();
         }
@@ -38,8 +47,12 @@ public class MainActivity extends AppCompatActivity {
             startActivity(i);
             finish();
         }
+        
+        String email = "adipopa@gmail.com";
+        getList(email);
 
         registerNotif = (TextView) findViewById(R.id.registerNotification);
+        switchButton = (Button) findViewById(R.id.switchButton);
 
         if(registerNotification.equals("show")){
             registerNotif.setVisibility(View.VISIBLE);
@@ -47,35 +60,29 @@ public class MainActivity extends AppCompatActivity {
         else{
             registerNotif.setVisibility(View.GONE);
         }
+        
+        mainList = (ListView) findViewById(R.id.mainList);
+        noLocks = (TextView) findViewById(R.id.noLocks);
 
-        Data data1 = new Data("Turda", "Str. Lotus 19");
-        Data data2 = new Data("Cluj-Napoca", "Str. Meteor 19");
-        Data data3 = new Data("Bucuresti", "Str. Basarabiei");
+        dataList = new ArrayList<>();
 
-        Data[] dataArray = {data1, data2, data3};
+        mainList.setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        ImageView listIcon = (ImageView) view.findViewById(R.id.listIcon);
+                        listIcon.setImageResource(R.mipmap.lockicon);
+                    }
+                }
+        );
 
-        ListAdapter CustomAdapter = new CustomAdapter(this, dataArray);
-        ListView myListView = (ListView) findViewById(R.id.mainList);
+    }
 
-        myListView.setAdapter(CustomAdapter);
-
-                myListView.setOnItemClickListener(
-                        new AdapterView.OnItemClickListener(){
-                            @Override
-                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                                ImageView listIcon = (ImageView) view.findViewById(R.id.listIcon);
-                                listIcon.setImageResource(R.mipmap.lockicon);
-                            }
-                        }
-                );
-        }
-
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            getMenuInflater().inflate(R.menu.menu_main, menu);
-            return true;
-        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
         @Override
         public boolean onOptionsItemSelected(MenuItem item){
@@ -109,6 +116,29 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onMenuOpened(featureId, menu);
     }
+    
+    public void onSwitch(View view){
+        String lock_inner_id = "B55HH471C28f646G";
+        String type = "switch";
+        LocksWorker locksWorker = new LocksWorker(this);
+        locksWorker.execute(type, lock_inner_id);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                switch (LocksWorker.lockStatus) {
+                    case "unlocked":
+                        Toast.makeText(MainActivity.this, "unlocked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case "locked":
+                        Toast.makeText(MainActivity.this, "locked", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "the lock was not found", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, 750);
+    }
 
     public void onHideNotification(View view){
         registerNotif.animate()
@@ -134,6 +164,62 @@ public class MainActivity extends AppCompatActivity {
                     public void onAnimationRepeat(Animator animator) {
                     }
                 });
+    }
+    
+    // Method to acquire the lock list from the database
+
+    public void getList(String email) {
+        String type = "getlist";
+        LocksWorker locksWorker = new LocksWorker(this);
+        locksWorker.execute(type, email);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                String cardinal = "";
+                int i = 0;
+                while (LocksWorker.list[i] != '#') {
+                    cardinal += LocksWorker.list[i];
+                    i++;
+                }
+                i++;
+                int cardinal_parse = Integer.parseInt(cardinal);
+                if (cardinal_parse > 0) {
+                    for (int n = 1; n <= cardinal_parse; n++) {
+                        String nickname = "";
+                        String shareID = "";
+                        String is_open = "";
+                        while (LocksWorker.list[i] != '#') {
+                            nickname += LocksWorker.list[i];
+                            i++;
+                        }
+                        i++;
+                        while (LocksWorker.list[i] != '#') {
+                            shareID += LocksWorker.list[i];
+                            i++;
+                        }
+                        i++;
+                        while (LocksWorker.list[i] != '#') {
+                            is_open += LocksWorker.list[i];
+                            i++;
+                        }
+                        i++;
+                        Data data = new Data(nickname, shareID, is_open);
+                        dataList.add(data);
+                    }
+                } else {
+                    noLocks.setVisibility(View.VISIBLE);
+                }
+
+                // 08.08.2016 - 22:51 : Am rezolvaaaaat, trebuia tot in handler... ca handlerul ruleaza in paralel cu interfata, Jeeeesus
+
+                simpleArray = new Data[dataList.size()];
+                dataList.toArray(simpleArray);
+
+                ListAdapter listAdapter = new LocksListAdapter(MainActivity.this, simpleArray);
+
+                mainList.setAdapter(listAdapter);
+            }
+        }, 1000);
     }
 
     // Method to require double press on the back button to close the app
