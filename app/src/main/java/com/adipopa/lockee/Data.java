4b9acbc4c18e0bd4b02231ea3ce8d38828f8cfcc
Adrian Popa <adipopa9910@gmail.com>
2016-08-19 20:35:11 +0300

    Added another string value to the class

--- app/src/main/java/com/adipopa/lockee/Data.java
+++ app/src/main/java/com/adipopa/lockee/Data.java
@@ -3,11 +3,13 @@ package com.adipopa.lockee;
 public class Data {
 
     public String nickname;
+    public String lockID;
     public String shareID;
     public String is_open;
 
-    public Data(String nickname, String shareID, String is_open) {
+    public Data(String nickname, String lockID, String shareID, String is_open) {
         this.nickname = nickname;
+        this.lockID = lockID;
         this.shareID = shareID;
         this.is_open = is_open;
     }
