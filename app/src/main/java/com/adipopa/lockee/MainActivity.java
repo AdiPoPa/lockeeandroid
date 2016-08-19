d1acf454dea042fb3f23f52a8522c7d878dd0c9b
Adrian Popa <adipopa9910@gmail.com>
2016-08-19 20:42:25 +0300

    Updated tags and ListView SimpleAdapter

--- app/src/main/java/com/adipopa/lockee/MainActivity.java
+++ app/src/main/java/com/adipopa/lockee/MainActivity.java
@@ -5,14 +5,13 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,10 +24,6 @@ import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.nhaarman.supertooltips.ToolTip;
-import com.nhaarman.supertooltips.ToolTipRelativeLayout;
-import com.nhaarman.supertooltips.ToolTipView;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -40,7 +35,7 @@ import java.util.HashMap;
 
 import dmax.dialog.SpotsDialog;
 
-public class MainActivity extends AppCompatActivity implements ToolTipView.OnToolTipViewClickedListener {
+public class MainActivity extends AppCompatActivity {
 
     public static String registerNotification = "hide";
     ListView mainList;
@@ -49,8 +44,10 @@ public class MainActivity extends AppCompatActivity implements ToolTipView.OnToo
 
     private static final String TAG_LOCKINFO = "locks_info";
     private static final String TAG_NICKNAME = "nickname";
+    private static final String TAG_LOCK_ID = "lock_inner_id";
     private static final String TAG_SHARE_ID = "share_id";
-    private static final String TAG_IS_OPENED = "is_opened";
+    private static final String TAG_STATUS = "status";
+    private static final String TAG_ICON = "icon";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,20 +67,10 @@ public class MainActivity extends AppCompatActivity implements ToolTipView.OnToo
 
             FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
-            ToolTip toolTip = new ToolTip()
-                    .withText(getString(R.string.registerGuide))
-                    .withTextColor(Color.WHITE)
-                    .withColor(ContextCompat.getColor(this, R.color.colorAccent))
-                    .withShadow()
-                    .withAnimationType(ToolTip.AnimationType.NONE);
-            ToolTipRelativeLayout toolTipRelativeLayout = (ToolTipRelativeLayout) findViewById(R.id.registerGuide);
-            final ToolTipView myToolTipView = toolTipRelativeLayout.showToolTipForView(toolTip, fab);
-            myToolTipView.setOnToolTipViewClickedListener(MainActivity.this);
-
             TextView registerSuccessful = (TextView)findViewById(R.id.registerNotification);
 
             if (registerNotification.equals("show")) {
-                // Custom animation on image
+                
                 final ObjectAnimator fadeIn = ObjectAnimator.ofFloat(registerSuccessful, "alpha", 0f, 1f);
                 fadeIn.setDuration(2000);
                 final ObjectAnimator fadeOut = ObjectAnimator.ofFloat(registerSuccessful, "alpha",  1f, 0f);
@@ -97,7 +84,7 @@ public class MainActivity extends AppCompatActivity implements ToolTipView.OnToo
                     @Override
                     public void onAnimationEnd(Animator animation) {
                         super.onAnimationEnd(animation);
-                        mAnimationSet.setStartDelay(5000);
+                        mAnimationSet.setStartDelay(3000);
                         mAnimationSet.play(fadeOut);
                         mAnimationSet.start();
                         new Handler().postDelayed(new Runnable(){
@@ -106,24 +93,39 @@ public class MainActivity extends AppCompatActivity implements ToolTipView.OnToo
                                 mAnimationSet.end();
                                 new getLocks().execute(email);
                             }
-                        }, 7000);
+                        }, 5000);
                     }
                 });
                 mAnimationSet.start();
             } else {
-                myToolTipView.remove();
-                new getLocks().execute(email);
                 registerSuccessful.setVisibility(View.GONE);
             }
+            
+            // TODO: Make the list gone when there are no locks
 
             mainList = (ListView) findViewById(R.id.mainList);
+            mainList.addFooterView(new View(MainActivity.this));
 
             mainList.setOnItemClickListener(
                     new AdapterView.OnItemClickListener() {
                         @Override
                         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                            ImageView listIcon = (ImageView) view.findViewById(R.id.listIcon);
-                            listIcon.setImageResource(R.mipmap.lockicon);
+                            TextView nicknameText = (TextView) view.findViewById(R.id.nicknameText);
+                            TextView lockIDText = (TextView) view.findViewById(R.id.lockIDText);
+                            TextView shareIDText = (TextView) view.findViewById(R.id.shareIDText);
+                            TextView statusText = (TextView) view.findViewById(R.id.statusText);
+                            Intent intent = new Intent(MainActivity.this, ControlActivity.class);
+
+                            String nickname = nicknameText.getText().toString();
+                            String lockID = lockIDText.getText().toString();
+                            String shareID = shareIDText.getText().toString();
+                            String status = statusText.getText().toString();
+                            intent.putExtra("nickname", nickname);
+                            intent.putExtra("lockID", lockID);
+                            intent.putExtra("shareID", shareID);
+                            intent.putExtra("status", status);
+
+                            startActivity(intent);
                         }
                     }
             );
@@ -131,7 +133,6 @@ public class MainActivity extends AppCompatActivity implements ToolTipView.OnToo
             fab.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    myToolTipView.remove();
                 }
             });
         }
@@ -173,11 +174,6 @@ public class MainActivity extends AppCompatActivity implements ToolTipView.OnToo
             }
         }
         return super.onMenuOpened(featureId, menu);
-    }
-
-    @Override
-    public void onToolTipViewClicked(ToolTipView toolTipView) {
-        toolTipView.remove();
     }
 
     /** Async task class to get json by making HTTP call **/
@@ -229,11 +225,11 @@ public class MainActivity extends AppCompatActivity implements ToolTipView.OnToo
 
                     if(locksList != null) {
                         if (locksList.size() > 0) {
-                            ListAdapter adapter = new SimpleAdapter(
+                           ListAdapter adapter = new SimpleAdapter(
                                     MainActivity.this, locksList,
-                                    R.layout.main_list, new String[]{TAG_NICKNAME, TAG_SHARE_ID, TAG_IS_OPENED},
-                                    new int[]{R.id.nicknameText, R.id.shareIDText, R.id.statusText});
-
+                                    R.layout.main_list, new String[]{TAG_NICKNAME, TAG_LOCK_ID, TAG_SHARE_ID, TAG_STATUS, TAG_ICON},
+                                    new int[]{R.id.nicknameText, R.id.lockIDText, R.id.shareIDText, R.id.statusText, R.id.lockIcon}
+                            );
                             mainList.setAdapter(adapter);
                         } else {
                             noLocks.setVisibility(View.VISIBLE);
@@ -263,16 +259,27 @@ public class MainActivity extends AppCompatActivity implements ToolTipView.OnToo
                     JSONObject c = locks.getJSONObject(i);
 
                     String nickname = c.getString(TAG_NICKNAME);
+                    String lock_id = c.getString(TAG_LOCK_ID);
                     String share_id = c.getString(TAG_SHARE_ID);
-                    String is_opened = c.getString(TAG_IS_OPENED);
+                    String status = "";
+                    int icon = 0;
+                    if(c.getString(TAG_STATUS).equals("true")) {
+                        status = "unlocked";
+                        icon = R.drawable.ic_lock_open_white_48dp;
+                    } else if (c.getString(TAG_STATUS).equals("false")){
+                        status = "locked";
+                        icon = R.drawable.ic_lock_outline_white_48dp;
+                    }
 
                     // tmp hashmap for single lock
                     HashMap<String, String> lock = new HashMap<>();
 
                     // adding each child node to HashMap key => value
                     lock.put(TAG_NICKNAME, nickname);
+                    lock.put(TAG_LOCK_ID, lock_id);
                     lock.put(TAG_SHARE_ID, share_id);
-                    lock.put(TAG_IS_OPENED, is_opened);
+                    lock.put(TAG_STATUS, status);
+                    lock.put(TAG_ICON, Integer.toString(icon));
 
                     // adding lock to locks list
                     locksList.add(lock);
@@ -287,28 +294,11 @@ public class MainActivity extends AppCompatActivity implements ToolTipView.OnToo
             return null;
         }
     }
-
-    public void onSwitch(View view){
-        String lock_inner_id = "B55HH471C28f646G";
-        String type = "switch";
-        LocksWorker locksWorker = new LocksWorker(this);
-        locksWorker.execute(type, lock_inner_id);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                switch (LocksWorker.lockStatus) {
-                    case "unlocked":
-                        Toast.makeText(MainActivity.this, "unlocked", Toast.LENGTH_SHORT).show();
-                        break;
-                    case "locked":
-                        Toast.makeText(MainActivity.this, "locked", Toast.LENGTH_SHORT).show();
-                        break;
-                    default:
-                        Toast.makeText(MainActivity.this, "the lock was not found", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        }, 750);
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new getLocks().execute(email);
     }
 
     // Method to require double press on the back button to close the app
