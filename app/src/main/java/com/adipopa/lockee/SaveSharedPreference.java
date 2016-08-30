862fe8a9d4addc71d982a7e9cfb245ba88359234
Adrian Popa <adipopa9910@gmail.com>
2016-08-30 23:17:18 +0300

    Added some custom shared preferences

--- app/src/main/java/com/adipopa/lockee/SaveSharedPreference.java
+++ app/src/main/java/com/adipopa/lockee/SaveSharedPreference.java
@@ -6,20 +6,40 @@ import android.preference.PreferenceManager;
 
 public class SaveSharedPreference {
     static final String PREF_LOGIN_STATUS = "logged in";
+    static final String PREF_SHARED_NICKNAME = "nickname";
+    static final String PREF_SHARED_ID = "shared";
 
     static SharedPreferences getSharedPreferences(Context ctx) {
         return PreferenceManager.getDefaultSharedPreferences(ctx);
     }
 
-    public static void setLoginStatus(Context ctx, String userName)
-    {
+    public static void setLoginStatus(Context ctx, String userName) {
         SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
         editor.putString(PREF_LOGIN_STATUS, userName);
         editor.apply();
     }
 
-    public static String getLoginStatus(Context ctx)
-    {
+    public static String getLoginStatus(Context ctx) {
         return getSharedPreferences(ctx).getString(PREF_LOGIN_STATUS, "not logged");
+    }
+
+    public static void setSharedNickname(Context ctx, String nickname){
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_SHARED_NICKNAME, nickname);
+        editor.apply();
+    }
+
+    public static String getSharedNickname(Context ctx) {
+        return getSharedPreferences(ctx).getString(PREF_SHARED_NICKNAME, "no nickname");
+    }
+
+    public static void setSharedID(Context ctx, String shareID){
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_SHARED_ID, shareID);
+        editor.apply();
+    }
+
+    public static String getSharedID(Context ctx) {
+        return getSharedPreferences(ctx).getString(PREF_SHARED_ID, "not shared");
     }
 }
