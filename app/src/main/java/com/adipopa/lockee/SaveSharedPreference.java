83c9bb79b82f1ce38bd52e2404a0955b9f346ba9
Adrian Popa <adipopa9910@gmail.com>
2016-10-04 18:04:17 +0300

    Update SaveSharedPreference.java

--- app/src/main/java/com/adipopa/lockee/SaveSharedPreference.java
+++ app/src/main/java/com/adipopa/lockee/SaveSharedPreference.java
@@ -1,34 +1,55 @@
+/****************************************************************************************
+ *                                                                                       *
+ *   Copyright (C) 2016 Glimpse Team                                                     *
+ *                                                                                       *
+ *       This file is part of the Lockee project and is hereby protected by copyright    *
+ *   and can not be copied and/or distributed without the express permission of all      *
+ *   the Glimpse Team members.                                                           *
+ *                                                                                       *
+ ****************************************************************************************/
+
 package com.adipopa.lockee;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-public class SaveSharedPreference {
-    static final String PREF_LOGIN_STATUS = "logged in";
-    static final String PREF_SHARED_ID = "shared";
+class SaveSharedPreference {
+    private static final String PREF_LOGIN_STATUS = "logged in";
+    private static final String PREF_SHARED_ID = "shared";
+    private static final String PREF_SHARED_NAME = "name";
 
-    static SharedPreferences getSharedPreferences(Context ctx) {
+    private static SharedPreferences getSharedPreferences(Context ctx) {
         return PreferenceManager.getDefaultSharedPreferences(ctx);
     }
 
-    public static void setLoginStatus(Context ctx, String userName) {
+    static void setLoginStatus(Context ctx, String userName) {
         SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
         editor.putString(PREF_LOGIN_STATUS, userName);
         editor.apply();
     }
 
-    public static String getLoginStatus(Context ctx) {
+    static String getLoginStatus(Context ctx) {
         return getSharedPreferences(ctx).getString(PREF_LOGIN_STATUS, "not logged");
     }
 
-    public static void setSharedID(Context ctx, String shareID){
+    static void setSharedID(Context ctx, String shareID){
         SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
         editor.putString(PREF_SHARED_ID, shareID);
         editor.apply();
     }
 
-    public static String getSharedID(Context ctx) {
+    static String getSharedID(Context ctx) {
         return getSharedPreferences(ctx).getString(PREF_SHARED_ID, "not shared");
+    }
+
+    static void setSharedName(Context ctx, String shareName){
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_SHARED_NAME, shareName);
+        editor.apply();
+    }
+
+    static String getSharedName(Context ctx) {
+        return getSharedPreferences(ctx).getString(PREF_SHARED_NAME, "no name");
     }
 }
